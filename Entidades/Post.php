9f0b8da31428717ddfca1fe93ef2c5457f36d957
adonai <adoargu@gmail.com>
2013-03-20 20:22:58 +0100

    He modificado la clase Post ya que la tendré que utilizar en la clase de prueba, DatosPrueba.php, he añadido todos los atributos de la base de datos, el constructor y los getters. Revisarlo y cualquier cosita me decis ;)

--- Entidades/Post.php
+++ Entidades/Post.php
@@ -1,8 +1,57 @@
 <?php
 
 class Post {
-    public function __construct() {
-        
+    
+    private $id;
+    private $idUsuario;
+    private $idCategoria;
+    private $titulo;
+    private $texto;
+    private $fechaCreacion;
+    private $fechaModificacion;
+    private $modificaciones;
+    
+    public function __construct($id, $idUsuario, $idCategoria, $titulo, $texto, $fechaCreacion, $fechaModificacion, $modificaciones) {
+        $this->id = $id;
+        $this->idUsuario = $idUsuario;
+        $this->idCategoria = $idCategoria;
+        $this->titulo = $titulo;
+        $this->texto = $texto;
+        $this->fechaCreacion = $fechaCreacion;
+        $this->fechaModificacion = $fechaModificacion;
+        $this->modificaciones = $modificaciones;
+    }
+    
+    public function getId(){
+        return $this->id;
+    }
+    
+    public function getIdUsuario(){
+        return $this->idUsuario;
+    }
+    
+    public function getIdCategoria(){
+        return $this->idCategoria;
+    }
+    
+    public function getTitulo(){
+        return $this->titulo;
+    }
+    
+    public function getTexto(){
+        return $this->texto;
+    }
+    
+    public function getFechaCreacion(){
+        return $this->fechaCreacion;
+    }
+    
+    public function getFechaModificacion(){
+        return $this->fechaModificacion;
+    }
+    
+    public function getModificaciones(){
+        return $this->modificaciones;
     }
 }
 
