4d1b60c617ae084100fef351d2bd4cecb75714dc
Rafa MacRod <rafagn91@gmail.com>
2013-04-02 19:51:18 +0200

    Esqueleto básico funciones validador
    Seguramente falte algún campo por validar, irme diciendo.
    Os lo dejo en true por si queréis ir trabajando con ellos,
    según los termine iré subiéndolos.

--- Helpers/Validador.php
+++ Helpers/Validador.php
@@ -3,6 +3,22 @@ class Validador {
     public function __construct() {
         
     }
+    # Esqueleto básico por si quereis empezar a trabajar
+    # TODO Rafa
+    public function validarNombreUsuario($nombreUsuario){
+        return true;
+    }
+    public function validarPass($pass){
+        return true;
+    }
+    public function validarMail($mail){
+        return true;
+    }
+    public function validarCategoria($categoria){
+        # Validaremos si la categoria existe en la bd antes de modificarla o borrarla
+        return true;
+    }
+    
 }
 
 ?>
