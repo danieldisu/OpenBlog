9a62de063ce451efc0648b739fc40cd1a795d73b
Daniel Diaz Suarez <ddiaz@autentia.com>
2013-04-22 11:54:08 +0200

    Añadido paginador.php

---
+++ Helpers/Paginador.php
@@ -0,0 +1,13 @@
+<?php
+
+Class Paginador{
+
+	/*
+		Funcion que te devuelve el numero del primer post de la pagina que le indiques, 
+	*/
+	public function getNumeroPrimerPostPagina($numeroPagina){
+
+	}
+}
+	
+?>
--- index.php
+++ index.php
@@ -1,100 +1,106 @@
 <?php
-    include_once 'Helpers/Paginacion.php';
-    include_once 'Helpers/ManejadorConfig.php';
-    include_once 'Helpers/ManejadorBD.php';
-    include_once './Entidades/Categoria.php';
-    include_once './Entidades/Rol.php';
-    include_once './Entidades/Usuario.php';
-    include_once './Entidades/Post.php';
-    include_once './Entidades/Comentario.php';
-    
-    $mj = new ManejadorConfig();
-    $json = $mj->cargarConfig();
-    
-    $mbd = new ManejadorBD();
+	include_once 'Helpers/Paginador.php';
+	include_once 'Helpers/ManejadorConfig.php';
+	include_once 'Helpers/ManejadorBD.php';
+	include_once './Entidades/Categoria.php';
+	include_once './Entidades/Rol.php';
+	include_once './Entidades/Usuario.php';
+	include_once './Entidades/Post.php';
+	include_once './Entidades/Comentario.php';
+	
+	$mj = new ManejadorConfig();
+	$json = $mj->cargarConfig();
+	
+	$mbd = new ManejadorBD();
 
-    $pagina = $_GET['p'];
-    if($pagina)
+	if(!empty($_GET['p']))
+		$pagina = $_GET['p'];
+	else
+		$pagina = 0;
+	
+	echo "Mostrando pagina ". $pagina;
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
-    <head>
+	<head>
 
-        <meta charset="utf-8">
-        <title>OpenBlog</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<title>OpenBlog</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <!-- Le styles -->
-        <link href="Recursos/css/bootstrap.css" rel="stylesheet">
-        <link href="Recursos/css/bootstrap-responsive.css" rel="stylesheet">
-        <link href="Recursos/css/fuentes.css" rel="stylesheet">
-        <link href=<?php echo $json["rutaCss"] ?> rel="stylesheet">
+		<!-- Le styles -->
+		<link href="Recursos/css/bootstrap.css" rel="stylesheet">
+		<link href="Recursos/css/bootstrap-responsive.css" rel="stylesheet">
+		<link href="Recursos/css/fuentes.css" rel="stylesheet">
+		<link href=<?php echo $json["rutaCss"] ?> rel="stylesheet">
 
-        <!-- Favicon -->
-        <link rel="shortcut icon" href="../recursos/ico/favicon.ico">
-    </head>
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="../recursos/ico/favicon.ico">
+	</head>
 
-    <body>
-      <div class="container">
+	<body>
+	  <div class="container">
 
-        <div class="row">
-            <div class="span12 header">
-                <img src="Recursos/betaLogo01.png">
-                <h1><?php echo $json["tituloBlog"] ?></h1>
-            </div>
-        </div><!-- /header -->
+		<div class="row">
+			<div class="span12 header">
+				<img src="Recursos/betaLogo01.png">
+				<h1><?php echo $json["tituloBlog"] ?></h1>
+			</div>
+		</div><!-- /header -->
 
-        <div class="row main">
-            <div class="span9 contenido">
-                <?php
-                    $posts = $mbd->obtenerUltimosPost(5,0);
-                    foreach ($posts as $post){
-                        include "Blog/plantillaPost.php";
-                    }
-                ?>
-                                   
-            </div>
-        <div class="span3 sidebar">
+		<div class="row main">
+			<div class="span9 contenido">
+				<?php
 
-            <div class="cajaLogin">
-              <h3> Login </h3>
-              <label>Usuario</label>
-              <input type="text" id="inputUsuario">
-              <label>Contraseña</label>
-              <input type="password" id="inputPassword">
-              <input type="button" class="btn btn-block" value="Login">
-              <input type="button" class="btn btn-block" value="Registrarse">
-            </div>
+					$posts = $mbd->obtenerUltimosPost(5, );
+					foreach ($posts as $post){
+					 include "Blog/plantillaPost.php";
+					}
+				?>
+									
+			</div>
+		<div class="span3 sidebar">
 
-            <div class="cajaUltimosPost">
-              <h3> Ultimos Post </h3>
-              <ul>
-                <li><a href="#">Post numero 1</a></li>
-                <li><a href="#">Post numero 2</a></li>
-                <li><a href="#">Post numero 3</a></li>
-                <li><a href="#">Post numero 4</a></li>
-                <li><a href="#">Post numero 5</a></li>
-              </ul>
-            </div>
+			<div class="cajaLogin">
+			  <h3> Login </h3>
+			  <label>Usuario</label>
+			  <input type="text" id="inputUsuario">
+			  <label>Contraseña</label>
+			  <input type="password" id="inputPassword">
+			  <input type="button" class="btn btn-block" value="Login">
+			  <input type="button" class="btn btn-block" value="Registrarse">
+			</div>
 
-            <div class="cajaUltimosComentarios">
-              <h3> Ultimos Comentarios </h3>
-              <ul>
-                <li>Por: <a href="#">Usuario</a><br>Comentario numero 1</li>
-                <li>Por: <a href="#">Usuario</a><br>Comentario numero 2</li>
-                <li>Por: <a href="#">Usuario</a><br>Comentario numero 3</li>
-                <li>Por: <a href="#">Usuario</a><br>Comentario numero 4</li>
-                <li>Por: <a href="#">Usuario</a><br>Comentario numero 5</li>
-              </ul>            
-            </div>
-          </div>
-        </div>
-      </div> <!-- /container -->
+			<div class="cajaUltimosPost">
+			  <h3> Ultimos Post </h3>
+			  <ul>
+				<li><a href="#">Post numero 1</a></li>
+				<li><a href="#">Post numero 2</a></li>
+				<li><a href="#">Post numero 3</a></li>
+				<li><a href="#">Post numero 4</a></li>
+				<li><a href="#">Post numero 5</a></li>
+			  </ul>
+			</div>
 
-      <!-- Le javascript
-      ================================================== -->
-      <!-- Placed at the end of the document so the pages load faster -->
-      <script src="Recursos/js/jquery.js"></script>
-      <script type="Recursos/js/bootstrap.js"></script>
-    </body>
+			<div class="cajaUltimosComentarios">
+			  <h3> Ultimos Comentarios </h3>
+			  <ul>
+				<li>Por: <a href="#">Usuario</a><br>Comentario numero 1</li>
+				<li>Por: <a href="#">Usuario</a><br>Comentario numero 2</li>
+				<li>Por: <a href="#">Usuario</a><br>Comentario numero 3</li>
+				<li>Por: <a href="#">Usuario</a><br>Comentario numero 4</li>
+				<li>Por: <a href="#">Usuario</a><br>Comentario numero 5</li>
+			  </ul>            
+			</div>
+		  </div>
+		</div>
+	  </div> <!-- /container -->
+
+	  <!-- Le javascript
+	  ================================================== -->
+	  <!-- Placed at the end of the document so the pages load faster -->
+	  <script src="Recursos/js/jquery.js"></script>
+	  <script type="Recursos/js/bootstrap.js"></script>
+	</body>
 </html>
