2cacbda2b0f4870ba9390660feb232d32a95c524
danieldisu <danieldiazsuarez@gmail.com>
2013-05-15 17:07:48 +0200

    Primer commit Nuevo Post

--- paneladmin/nuevaEntrada.php
+++ paneladmin/nuevaEntrada.php
@@ -1,21 +1,40 @@
-<div class="cajaAdministracion row">
-   <div class="span12 cajaTituloCategoria">
-      <h2>Nueva Entrada</h2>
-   </div>
-   <div class="span12 cajaContenidoCategoria">
-      <p>
-         Aqui es donde tiene que ir el plugin de jQuery con un bonito editor de texto
-      </p>
-      <div class="cajaEditorTexto">
-        <form>
-          <textarea>ejemplo</textarea>
-        </form>
-      </div>
-      <button class="btn">Publicar Entrada</button>
-      <button class="btn">Limpiar</button>
-   </div>
-</div>
-
 <?php
-  
+include '../autoloader.php';
+
+use src\helpers\ManejadorBD;
+
+$mbd = new ManejadorBD();
 ?>
+
+<div class="cajaAdministracion row" id="cajaNuevoPost">
+  <div class="span12 cajaTituloCategoria">
+	<h2>Nueva Entrada</h2>
+  </div>
+  <div class="span12 cajaContenidoCategoria">
+	<form method="POST" action="paneladmin/nuevoPost.php">
+      <div class="cajaTitulo">
+        <label>Titulo:</label><input name="titulo" type="text" />
+      </div>
+     <div class="cajaEditorTexto">
+      <label>Texto:</label>
+		  <textarea name="texto"></textarea>
+      </div>
+	  <div class="cajaCategoria">
+		<label>Categoria: </label>
+		<select name="categoria">
+		  <?php
+		  $categorias = $mbd->getAllCategorias();
+		  foreach ($categorias as $categoria) {
+			echo "<option value='".$categoria->getId()."'>";
+			echo $categoria->getNombre();
+			echo "</option>";
+		  }
+		  ?>
+		</select>
+	  </div>
+	
+	<button class="btn">Publicar Entrada</button>
+	<input type="reset" class="btn" value="Limpiar">
+	</form>
+  </div>
+</div>
--- paneladmin/nuevoPost.php
+++ paneladmin/nuevoPost.php
@@ -1,5 +1,18 @@
 <?php
 /*
  * Pagina que se encargará de añadir un nuevo post a la base de datos
+ * TODO:
+ *	-Limpiar Post
+ *	-Añadir Categoria
+ *	-Mostrar Post una vez insertado
+ *	-Añadir Titulo
+ *	-Añadir Usuario
  */
+
+echo "<pre>";
+  print_r($_POST);
+echo "</pre>";
+
+  
+
 ?>
