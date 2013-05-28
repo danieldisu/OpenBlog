0c0c87195a4701a19a61aa762af472b8154bef14
Adonai <adoargu@gmail.com>
2013-05-28 23:24:18 +0200

    Modificaciones

--- src/templates/postEnLista.php
+++ src/templates/postEnLista.php
@@ -1,16 +1,19 @@
+<?php
+    use src\helpers\pathGen;
+?>
 <div class="post">
 	<div class="cajaTituloPost">
 	  <h3><?php echo $post->getTitulo(); ?></h3>	  	  
 	</div>
 	<div class="cajaInfo row">
 	  <div class="cajaAutorPost span2">
-	    <span><?php echo $mbd->obtenerNombreAutor($post->getIdUsuario()); ?></span>
+              <span><?php echo "<a href='".pathGen::pathPostUsuario($post->getIdUsuario())."'>".$mbd->obtenerNombreAutor($post->getIdUsuario())."</a>" ?></span>
 	  </div>
 	  <div class="cajaFechaPost span2 ">
 	    <span><?php echo $post->getFechaCreacion(); ?></span>
 	  </div>
 	  <div class="cajaCategoriaPost span2 ">
-	    <span><?php echo $mbd->obtenerNombreCategoria($post->getIdCategoria()); ?></span>
+              <span><?php echo "<a href='".pathGen::pathVerCategoria($post->getIdCategoria())."'>".$mbd->obtenerNombreCategoria($post->getIdCategoria())."</a>" ?></span>
 	  </div>
 	  <div class="cajaNumeroComentarios span2 ">
 	    <span><?php echo "Nº de comentarios: ".$mbd->obtenerNumeroComentarios($post->getId()); ?></span>
